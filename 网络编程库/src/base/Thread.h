e6d24e4867835e42514674b7eb9b67e8df69c3fc
zhanpengqiu <1219300839@qq.com>
2025-02-25 14:00:38 +0800

    add web coding

---
+++ 网络编程库/src/base/Thread.h
@@ -0,0 +1,44 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <thread>
+#include <functional>
+#include <memory>
+#include <string>
+#include <atomic>
+#include "noncopyable.h"
+
+class Thread:noncopyable{
+public:
+    // 定义线程function，使用function根甘泉的赋值
+    using ThreadFunc=std::function<void()>;
+    
+    explicit Thread(ThreadFunc,const std::string &name= std::string());
+    ~Thread();
+
+    void start();
+
+    void join();
+
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string& name() const { return name_; }
+
+    static int numCreated() { return numCreated_; }
+
+private:
+    void setDefaultName();  // 设置线程名
+
+    bool started_;  // 是否启动线程
+    bool joined_;   // 是否等待该线程
+    std::shared_ptr<std::thread> thread_;
+    pid_t tid_;     // 线程tid
+    // Thread::start() 调用的回调函数
+    // 其实保存的是 EventLoopThread::threadFunc()
+    ThreadFunc func_;   
+    std::string name_;  // 线程名
+    static std::atomic_int32_t numCreated_; // 线程索引
+};
+
+
+#endif // THREAD
