8628a5e7a245f2e97dd54ac16e31466b1270bb8e
zhanpengqiu <1219300839@qq.com>
2025-03-01 15:13:52 +0800

    add annotations

--- 网络编程库/src/base/ThreadPool.cc
+++ 网络编程库/src/base/ThreadPool.cc
@@ -18,6 +18,8 @@ ThreadPool::~ThreadPool(){
     }
 }
 
+/// @brief 启动线程池，并且将runInThread加入到线程中执行
+///        并且启动线程初始化回调函数
 void ThreadPool::start(){
     running_ =true;
     threads_.reserve(threadSize_);
@@ -40,7 +42,7 @@ void ThreadPool::start(){
 void ThreadPool::stop(){
     std::lock_guard<std::mutex> lock(mutex_);
     running_ = false;
-    cond_.notify_all();  //通知所有线程
+    cond_.notify_all();  //通知所有线程结束了
 }
 
 size_t ThreadPool::queueSize() const{
@@ -51,7 +53,7 @@ size_t ThreadPool::queueSize() const{
 void ThreadPool::add(ThreadFunction function){
     std::unique_lock<std::mutex> lock(mutex_);
     queue_.push_back(function);
-    cond_.notify_one();  // 通知一个线程
+    cond_.notify_one();  // 通知一个线程,有一个任务来了
 }
 
 void ThreadPool::runInThread(){
@@ -61,15 +63,19 @@ void ThreadPool::runInThread(){
         }
         ThreadFunction task;
 
+        // 设置while true让线程一直执行不退出
         while(true){
             {
                 std::unique_lock<std::mutex> lock(mutex_);
+                // 如果任务队列为空，那么启用条件变量进行等待操作
+                // 如果线程池调用了stop操作，那么这个running就会暂停，并且推出线程执行程序
                 while(queue_.empty()){
                     if(!running_){
                         return ;
                     }
                     cond_.wait(lock);
                 }
+                // 从任务池中取出一个任务进行执行
                 task=queue_.front();
                 queue_.pop_front();
             }
